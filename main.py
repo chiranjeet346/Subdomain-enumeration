154c15c1c522bfb9f4fe5538683e0d9456242749
Chiranjeet Kumar <93564557+chiranjeet346@users.noreply.github.com>
2023-09-26 19:21:11 +0530

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import requests
 import sys
-
 subdomains = open("subdomains.txt").read().splitlines()
+target = sys.argv[1]
+targ = target.split(",")
+for tar in targ:
+    for sub in subdomains:
+        sub_domain = f"http://{sub}.{tar}"
+        try:
+            requests.get(sub_domain)
+        except requests.ConnectionError:
+            pass
 
-for sub in subdomains:
-    sub_domain = f"http://{sub}.{sys.argv[1]}"
-    try:
-        requests.get(sub_domain)
-    except requests.ConnectionError:
-        pass
+        else:
+            print("Valid Domain: ", sub_domain)
+        
+    print("-----------------------------------------\n")
 
-    else:
-        print("Valid Domain: ", sub_domain)
